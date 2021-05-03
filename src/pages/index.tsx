187dcc0b76fcd6d5c8c511f41022291b071f2ff8
Ferreira94 <luciano_sls@live.com>
2021-05-02 21:34:47 -0300

    -Adding context -Added audio functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR';
+import { useContext } from 'react';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
@@ -21,11 +23,13 @@ type Episode = {
 }
 
 type HomeProps = {
-  latestEpisodes: Array<Episode>;
-  allEpisodes: Array<Episode>;
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext);
+
   return (
     <div className={styles.homePage}>
       <section className={styles.latestEpisodes}>
@@ -52,7 +56,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episódio"/>
                 </button>
               </li>
